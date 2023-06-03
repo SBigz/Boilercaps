8a1dcf302416baa902b95b9708705c99d84b2836
SBigz <Sacha.bigou@gmail.com>
2023-06-03 18:24:21 +0200

    backend add

--- Capsulator.js
+++ Capsulator.js
@@ -2,6 +2,8 @@
 
 import inquirer from "inquirer";
 import { execSync } from "child_process";
+import { join } from "path";
+import fs from "fs";
 
 // Couleurs pour les messages.
 const colors = {
@@ -13,6 +15,129 @@ const colors = {
   whiteText: "\x1b[37m",
   whiteBackground: "\x1b[47m",
   blackText: "\x1b[30m",
+};
+
+// Dépendances pour chaque type de projet
+const dependencies = {
+  react: [
+    "@fortawesome/fontawesome-svg-core",
+    "@fortawesome/free-solid-svg-icons",
+    "@fortawesome/react-fontawesome",
+    "@reduxjs/toolkit",
+    "antd",
+    "formik",
+    "moment",
+    "react",
+    "react-dom",
+    "react-moment",
+    "react-router-dom",
+    "styled-components",
+    "socket.io-client",
+    "uuid",
+  ],
+  "react-native": [
+    "@fortawesome/fontawesome-svg-core",
+    "@fortawesome/free-solid-svg-icons",
+    "@fortawesome/react-fontawesome",
+    "@reduxjs/toolkit",
+    "@react-native-async-storage/async-storage",
+    "@react-native-community/datetimepicker",
+    "@react-native-community/picker",
+    "@react-native-community/slider",
+    "@react-navigation/bottom-tabs",
+    "@react-navigation/native",
+    "@react-navigation/native-stack",
+    "antd",
+    "formik",
+    "lottie-react-native",
+    "moment",
+    "react",
+    "react-dom",
+    "react-native",
+    "react-native-app-intro-slider",
+    "react-native-gesture-handler",
+    "react-native-get-random-values",
+    "react-native-keyboard-aware-scroll-view",
+    "react-native-modal",
+    "react-native-reanimated",
+    "react-native-safe-area-context",
+    "react-native-screens",
+    "react-native-svg",
+    "react-native-vector-icons",
+    "react-redux",
+    "react-router-dom",
+    "rn-glitch-effect",
+    "styled-components",
+    "socket.io-client",
+    "toggle-switch-react-native",
+    "uuid",
+  ],
+  expo: [
+    "@fortawesome/fontawesome-svg-core",
+    "@fortawesome/free-solid-svg-icons",
+    "@fortawesome/react-fontawesome",
+    "@reduxjs/toolkit",
+    "@expo/webpack-config",
+    "@reduxjs/toolkit",
+    "@react-native-async-storage/async-storage",
+    "@react-native-community/datetimepicker",
+    "@react-native-community/picker",
+    "@react-native-community/slider",
+    "@react-navigation/bottom-tabs",
+    "@react-navigation/native",
+    "@react-navigation/native-stack",
+    "antd",
+    "formik",
+    "expo",
+    "expo-camera",
+    "expo-checkbox",
+    "expo-font",
+    "expo-image-picker",
+    "expo-linear-gradient",
+    "expo-splash-screen",
+    "expo-status-bar",
+    "expo-updates",
+    "lottie-react-native",
+    "moment",
+    "react",
+    "react-dom",
+    "react-moment",
+    "react-native",
+    "react-native-app-intro-slider",
+    "react-native-gesture-handler",
+    "react-native-get-random-values",
+    "react-native-keyboard-aware-scroll-view",
+    "react-native-modal",
+    "react-native-reanimated",
+    "react-native-safe-area-context",
+    "react-native-screens",
+    "react-native-svg",
+    "react-native-vector-icons",
+    "react-redux",
+    "react-router-dom",
+    "rn-glitch-effect",
+    "styled-components",
+    "socket.io-client",
+    "toggle-switch-react-native",
+    "uuid",
+  ],
+  next: [
+    "@fortawesome/fontawesome-svg-core",
+    "@fortawesome/free-solid-svg-icons",
+    "@fortawesome/react-fontawesome",
+    "@reduxjs/toolkit",
+    "antd",
+    "formik",
+    "moment",
+    "next",
+    "react",
+    "react-dom",
+    "react-moment",
+    "react-router-dom",
+    "styled-components",
+    "socket.io-client",
+    "uuid",
+  ],
 };
 
 // Fonction qui vérifie si une commande est installée sur le système.
@@ -48,6 +173,8 @@ async function ensureCommandExists(command) {
   }
 }
 
+let backendProjectName;
+
 async function main() {
   // Affiche un message de bienvenue.
   console.log(`\n${colors.redMatrix + colors.bright}
@@ -62,7 +189,7 @@ async function main() {
 ░ ░            ░  ░               ░     ░         ░  ░     ░  ░            ░ ░     ░     
 ░                                                                                        \n`);
 
-  const { projectName, projectType, dependencies } = await inquirer.prompt([
+  const { projectName, projectType } = await inquirer.prompt([
     {
       type: "input",
       name: "projectName",
@@ -73,67 +200,11 @@ async function main() {
       type: "list",
       name: "projectType",
       message: "Quel type de projet voulez-vous créer ?",
-      choices: ["react", "react-native", "expo"],
-    },
-    {
-      type: "checkbox",
-      name: "dependencies",
-      message: "Quelles dépendances supplémentaires voulez-vous installer ?",
-      choices: [
-        "@expo/webpack-config",
-        "@fortawesome/fontawesome-svg-core",
-        "@fortawesome/free-solid-svg-icons",
-        "@fortawesome/react-native-fontawesome",
-        "@react-native-async-storage/async-storage",
-        "@react-native-community/datetimepicker",
-        "@react-native-community/picker",
-        "@react-native-community/slider",
-        "@react-navigation/bottom-tabs",
-        "@react-navigation/native",
-        "@react-navigation/native-stack",
-        "@reduxjs/toolkit",
-        "expo",
-        "expo-camera",
-        "expo-checkbox",
-        "expo-font",
-        "expo-image-picker",
-        "expo-linear-gradient",
-        "expo-splash-screen",
-        "expo-status-bar",
-        "expo-updates",
-        "jest",
-        "lottie-react-native",
-        "moment",
-        "react",
-        "react-dom",
-        "react-moment",
-        "react-native",
-        "react-native-app-intro-slider",
-        "react-native-gesture-handler",
-        "react-native-get-random-values",
-        "react-native-keyboard-aware-scroll-view",
-        "react-native-modal",
-        "react-native-reanimated",
-        "react-native-safe-area-context",
-        "react-native-screens",
-        "react-native-svg",
-        "react-native-vector-icons",
-        "react-redux",
-        "rn-glitch-effect",
-        "socket.io-client",
-        "toggle-switch-react-native",
-        "uuid",
-      ],
+      choices: ["react", "react-native", "expo", "next"],
     },
   ]);
 
-  const defaultDeps = {
-    react: ["react", "react-dom"],
-    "react-native": ["react", "react-native"],
-    expo: ["react", "react-native", "expo"],
-  };
-
-  const projectDeps = defaultDeps[projectType].concat(dependencies);
+  const projectDeps = dependencies[projectType];
 
   console.log(
     `\n${
@@ -141,11 +212,15 @@ async function main() {
     }╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ Création d'un nouveau projet ${projectType} nommé ${projectName}...\n`
   );
 
-  await ensureCommandExists(`create-${projectType}-app`);
-
-  execSync(`npx create-${projectType}-app ${projectName}`, {
-    stdio: "inherit",
-  });
+  if (projectType === "next") {
+    await ensureCommandExists("create-next-app");
+    execSync(`npx create-next-app ${projectName}`, { stdio: "inherit" });
+  } else {
+    await ensureCommandExists(`create-${projectType}-app`);
+    execSync(`npx create-${projectType}-app ${projectName}`, {
+      stdio: "inherit",
+    });
+  }
 
   console.log(
     `\n${
@@ -153,13 +228,160 @@ async function main() {
     }(づ ￣ ³￣)づ Installation des dépendances...\n`
   );
 
-  for (const dep of projectDeps) {
+  const { selectedDeps } = await inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "selectedDeps",
+      message: "Quelles dépendances supplémentaires voulez-vous installer ?",
+      choices: projectDeps,
+    },
+  ]);
+
+  for (const dep of selectedDeps) {
     console.log(
       `\n\n${
         colors.redMatrix + colors.bright
       }Installation de ${dep}... (˵ ͡° ͜ʖ ͡°˵)\n`
     );
     execSync(`cd ${projectName} && npm install ${dep}`, { stdio: "inherit" });
+  }
+
+  // Fonction pour générer le fichier connection.js
+  function generateConnectionFile(projectName) {
+    const content = `
+const mongoose = require('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING;
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
+`;
+
+    const dirPath = join(process.cwd(), projectName, "models");
+    const filePath = join(dirPath, "connection.js");
+
+    // Créer le répertoire 'models' si il n'existe pas
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+
+    fs.writeFileSync(filePath, content);
+  }
+
+  const backendDeps = [
+    "cors",
+    "node-fetch@2",
+    "dotenv",
+    "jest",
+    "supertest",
+    { name: "mongoose", postInstall: generateConnectionFile },
+  ];
+
+  const { installBackend } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "installBackend",
+      message: "Voulez-vous installer un backend (Express) ?",
+      default: false,
+    },
+  ]);
+
+  if (installBackend) {
+    // Assurez-vous que 'express-generator' est installé
+    await ensureCommandExists("express-generator");
+
+    console.log(
+      `\n\n${
+        colors.redMatrix + colors.bright
+      }Création du squelette de l'application Express... (˵ ͡° ͜ʖ ͡°˵)\n`
+    );
+
+    // Demander le nom du projet backend
+    const { backendProjectName: projectName } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "backendProjectName",
+        message: "Quel est le nom de votre projet backend ?",
+        validate: (input) => !!input,
+      },
+    ]);
+
+    backendProjectName = projectName;
+
+    // Créer le répertoire pour le backend
+    fs.mkdirSync(join(process.cwd(), backendProjectName));
+
+    // Générer le projet Express dans ce répertoire
+    execSync(`cd ${backendProjectName} && express --no-view`, {
+      stdio: "inherit",
+    });
+
+    const { selectedBackendDeps } = await inquirer.prompt([
+      {
+        type: "checkbox",
+        name: "selectedBackendDeps",
+        message: "Quelles dépendances de backend voulez-vous installer ?",
+        choices: backendDeps.map((dep) =>
+          typeof dep === "string" ? dep : dep.name
+        ),
+      },
+    ]);
+
+    for (const dep of backendDeps) {
+      const depName = typeof dep === "string" ? dep : dep.name;
+      if (selectedBackendDeps.includes(depName)) {
+        console.log(
+          `\n\n${
+            colors.redMatrix + colors.bright
+          }Installation de ${depName}... (˵ ͡° ͜ʖ ͡°˵)\n`
+        );
+        execSync(`cd ${backendProjectName} && npm install ${depName}`, {
+          stdio: "inherit",
+        });
+
+        if (dep.postInstall) {
+          dep.postInstall(backendProjectName);
+        }
+      }
+    }
+  }
+
+  const { deployVercel } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "deployVercel",
+      message: "Voulez-vous déployer sur Vercel ?",
+      default: false,
+    },
+  ]);
+
+  if (deployVercel) {
+    const vercelConfig = `
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "app.js",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "app.js"
+    }
+  ]
+}
+`;
+    const filePath = join(process.cwd(), backendProjectName, "vercel.json");
+    fs.writeFileSync(filePath, vercelConfig);
+
+    console.log(
+      `\n${
+        colors.redMatrix + colors.bright + colors.underscore
+      }Fichier vercel.json généré avec succès !\n`
+    );
   }
 
   console.log(
